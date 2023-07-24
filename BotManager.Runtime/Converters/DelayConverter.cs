53eb975b5dcfa2466a1286ad4920036507b775a6
David Schulte <mail@david-schulte.de>
2023-07-24 22:39:11 +0200

    Added a `Delay` expression.

---
+++ BotManager.Runtime/Converters/DelayConverter.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+using BotManager.Runtime.Expressions;
+
+namespace BotManager.Runtime.Converters;
+
+/// <summary>
+/// The <see cref="JsonConverter"/> for <see cref="Delay"/>.
+/// </summary>
+public class DelayConverter : ExpressionConverterBase<Delay>
+{
+    /// <inheritdoc />
+    protected override Delay From(IExpression? value) => new(value);
+
+    /// <inheritdoc />
+    protected override IExpression? To(Delay value) => value.Milliseconds;
+}
---
+++ BotManager.Runtime/Expressions/Delay.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel;
+using BotManager.Runtime.Converters;
+
+namespace BotManager.Runtime.Expressions;
+
+/// <summary>
+/// Holds execution and waits the defined number of milliseconds.
+/// <para>
+/// Returns: <c>null</c>.
+/// </para>
+/// </summary>
+[TypeConverter(typeof(DelayConverter))]
+public class Delay : IExpression
+{
+    /// <summary>
+    /// Empty constructor for serialization.
+    /// </summary>
+    public Delay()
+    {
+    }
+    
+    /// <summary>
+    /// Creates an expression that holds execution and waits the defined number of milliseconds.
+    /// </summary>
+    /// <param name="milliseconds">The expression to resolve the number of milliseconds to wait.</param>
+    public Delay(IExpression? milliseconds)
+    {
+        Milliseconds = milliseconds;
+    }
+    
+    /// <summary>
+    /// Gets and sets the expression to resolve the number of milliseconds to wait.
+    /// </summary>
+    public IExpression? Milliseconds { get; set; }
+
+    /// <inheritdoc />
+    public async Task<object?> ExecuteAsync(RuntimeContext context, Type? returnType)
+    {
+        // Don't wait
+        if (Milliseconds is null)
+            return null;
+
+        // Resolve the time
+        var milliseconds = await context.ExecuteAsync<int>(Milliseconds);
+        
+        await Task.Delay(milliseconds);
+        return null;
+    }
+}
